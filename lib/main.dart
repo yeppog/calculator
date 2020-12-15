d50f555fc35c5e6f98d3ea0b1eadd05209982caf
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2020-12-16 02:43:31 +0800

    Fixed parsing issue.

--- lib/main.dart
+++ lib/main.dart
@@ -28,117 +28,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // app state deprecated
-  // String output = "0";
-  // String _output = "0";
-  // double num1 = 0.0;
-  // double num2 = 0.0;
-  // String operand = "";
-  // function to handle each button press
-  // Takes in the buttonText as a parameter
-// Old button pressed function that parses the input one by one.
-//   buttonPressedDeprecated(String buttonText) {
-//     switch(buttonText) {
-//       case "CLEAR":
-//         {
-//           _output = "0";
-//           output = "0";
-//           num1 = 0.0;
-//           num2 = 0.0;
-//           operand = "";
-//         } break;
-//       case "+":
-//       case "-":
-//       case "X":
-//       case "/":
-//         {
-//           num1 = double.parse(output);
-//           operand = buttonText;
-//           _output = "0";
-//         }
-//         break;
-//       case ".":
-//         {
-//           if (_output.contains(".")) {
-//             print("Already has a decimal in the output");
-//           } else {
-//             _output += buttonText;
-//           }
-//         } break;
-//       case "=": // if equals, we would evaluate the
-//         {
-//           num2 = double.parse(output);
-//
-//           switch (operand) {
-//             case "+":
-//               {
-//                 _output = (num1 + num2).toString();
-//               }
-//               break;
-//             case "-":
-//               {
-//                 _output = (num1 - num2).toString();
-//               }
-//               break;
-//             case "X":
-//               {
-//                 _output = (num1 * num2).toString();
-//               }
-//               break;
-//             case "/":
-//               {
-//                 _output = (num1 / num2).toString();
-//               }
-//               break;
-//           }
-//           print(_output);
-//           num1 = 0.0;
-//           num2 = 0.0;
-//           operand = "";
-//         } break;
-//
-//       default:
-//         _output += buttonText;
-//     }
-//
-//     print(_output);
-//
-//     setState(() {
-//       output = double.parse(_output).toStringAsFixed(2);
-//     });
-//
-//   }
-
   String output = "0";
-  String _output;
-  List<String> outputArr = List<String>();
+  String _output = "";
   String tempStore = "0";
-
+  // check if there is a need to set output to "0"
+  int reset;
 
   void buttonPressed(String buttonText) {
     switch(buttonText) {
       case "CLEAR":
         {
-          _output = null;
+          _output = "";
           output = "0";
-          tempStore = null;
-          outputArr = List<String>();
+          reset = 1;
         } break;
       case "+":
       case "-":
       case "X":
       case "/":
         {
-          if (tempStore != null) {
-            outputArr.add(tempStore);
-            tempStore = null;
-          }
-
-          if (outputArr.last == "+" || outputArr.last == "-" || outputArr.last == "/" || outputArr.last == "X") {
-            outputArr.last = buttonText;
+          String checker = _output[_output.length - 1];
+          if (checker == "+" || checker == "-" || checker == "X" || checker == "/") {
             _output = _output.substring(0, _output.length - 1) + buttonText;
           } else {
-            outputArr.add(buttonText);
             _output += buttonText;
           }
         }
@@ -150,46 +62,47 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
       case "DEL":
         {
-          if (outputArr.length != 0) {
-            if (outputArr.last == "+" || outputArr.last == "-" || outputArr.last == "/" || outputArr.last == "X") {
-              outputArr.removeLast();
-              _output = _output.substring(0, _output.length - 1);
-            }
-          }
-          else if (_output != null ) {
-            tempStore = tempStore.substring(0, tempStore.length - 1);
-            _output = _output.substring(0, _output.length - 1);
-          }
+          _output = _output.substring(0, _output.length - 1);
         } break;
       default:
-        if (tempStore == null) {
-          tempStore = buttonText;
-        } else {
-          tempStore += buttonText;
-        } if (_output == null) {
-          _output = buttonText;
-        } else {
-          _output += buttonText;
-        }
+          { _output += buttonText; }
+          break;
     }
     // debug
-    print(_output);
-    print(outputArr);
+    print(parseString(_output));
 
     setState(() {
-      if (_output != null) {
-        output = _output;
-      } else if (_output == "") {
+      if (reset == 1) {
         output = "0";
+        reset = 0;
+      } else {
+        output = _output;
       }
     });
 
   }
 
 
-  parseInput(String input) {
 
+
+  List parseString (String input) {
+    String store = "";
+    List<String> outputArr = new List<String>();
+    List<String> operatorArr = ["+", "-", "/", "X", "(", ")"];
+    for (int i = 0; i < input.length; i ++ ) {
+      if (operatorArr.contains(input[i])) {
+        if (store.length != 0) {
+          outputArr.add(store);
+          store = "";
+        }
+        outputArr.add(input[i]);
+      } else {
+        store += input[i];
+      }
+    }
+    return outputArr;
   }
+
   // Widget for each button, calls out the onPressed function which updates the
   // state of the calculator
   Widget buildButton(String buttonText) {
